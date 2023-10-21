94aee3d33e8d437f104ef4318ca5c9bb29e84e47
tussakin <131391869+tussakin@users.noreply.github.com>
2023-10-21 15:40:40 +0200

    Redigerad och kommenterad

--- OOPAnimals/Tiger.cs
+++ OOPAnimals/Tiger.cs
@@ -1,39 +1,57 @@
-﻿using System;
-namespace OOPAnimals
+﻿namespace OOPAnimals;
+
+// Internal class eftersom det bara behöver nås inom programmet. Defaultvärden sätts, samt deklaration av unikt fält. 
+internal class Tiger : Animal
 {
-  class Tiger : Animal
+    public Tiger(string animalType = "Tiger", string colour = "orange", string name = "Tony", int age = 24,
+        bool carnivore = true, string habitat = "CerealBoxes", string favouriteCereal = "Kellog's frosted flakes")
+        : base(animalType, colour, name, age, carnivore, habitat)
     {
-        private string FavouriteCereal { get; set; }
+        FavouriteCereal = favouriteCereal;
+    }
+
+    private string FavouriteCereal { get; }
+
+    // Unik metod för klassen som frågar användaren efter specifik input, och svarar lämpligt efter input. 
+    public void PourCereal()
+    {
+        while (true)
+        {
+            var hungryUser = Console.ReadLine();
 
 
-        public Tiger(string animalType = "Tiger", string colour="orange", string name="Tony", int age=24, 
-            bool carnivore=true, string habitat="CerealBoxes", string favouriteCereal="Kellogs frosted flakes")
-            : base(animalType, colour, name, age, carnivore, habitat)
-        {
-            
-            FavouriteCereal = favouriteCereal;
-        }
+            if (hungryUser == "y")
+            {
+                Console.WriteLine("Tigern tycker att du ser lite hungrig ut, du du får en skål flingor.");
+                break;
+            }
 
-        
-        public void PourCereal()
-        {
-            Console.WriteLine("Tigern tycker att du ser lite hungrig ut, du du får en skål flingor.");
-        }
-        
-        public void MakeSound()
-        {
-            Console.WriteLine("Grrrreat säger tigern!");
-            
-            
-        }
-        
-        public void PrintTiger()
-        {
-            Console.WriteLine($"Mina favoritflingor är {FavouriteCereal}!");
+            if (hungryUser == "n")
+            {
+                Console.WriteLine("Du får inga flingor.");
+                break;
+            }
+
+            Console.WriteLine("Du måste svara 'y' eller 'n'.");
         }
     }
+
+    /* MakeSound metod som tar in input från användaren och skriver ut passande statements för vad den säger, beroende
+     på om inputen är tom eller inte. */
+    public void MakeSound()
+    {
+        Console.WriteLine("Skriv in hur tigern låter:");
+
+        var tigerSound = Console.ReadLine();
+
+        Console.WriteLine(tigerSound == null
+            ? "Tigern är ganska tyst idag."
+            : $"{tigerSound} låter tigern!");
+    }
+
+    // Skriver ut det unika fältet i klassen. 
+    public void PrintTiger()
+    {
+        Console.WriteLine($"Mina favoritflingor är {FavouriteCereal}!");
+    }
 }
-
-
-
-
